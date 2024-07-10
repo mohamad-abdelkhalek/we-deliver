d6627ce1490456dde7cfd4c7716f72d915c2cae3
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-07-10 20:17:09 +0300

    Print first menu function <Added>

---
+++ main.py
@@ -0,0 +1,6 @@
+def firstMenu():
+    print("Hello! Please enter:\n"
+          "\t1. To go to the drivers' menu\n"
+          "\t2. To go to the cities' menu\n"
+          "\t3. To exit the system")
+    
