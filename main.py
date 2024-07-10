df97a76c54dce237f9c523eada2980f314fc7ee9
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-07-10 20:21:03 +0300

    Print drivers menu function <Added>

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ def firstMenu():
           "\t2. To go to the cities' menu\n"
           "\t3. To exit the system")
     
+def driversMenu():
+    print("Enter:\n"
+          "\t1. To view all the drivers\n"
+          "\t2. To add a driver\n"
+          "\t3. To go back to main menu")
+    
