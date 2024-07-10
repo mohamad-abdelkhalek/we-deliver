0e551e2a7a8ec89424ea5f43bd3cfc60dcb08ee2
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-07-10 20:59:12 +0300

    <Add> function to import drivers data from drivers.txt

--- main.py
+++ main.py
@@ -16,3 +16,21 @@ def citiesMenu():
           "\t2. To print neighboring cities\n"
           "\t3. To print drivers delivering to city")
     
+# Function to import drivers from the text file into the list as a form of list of dictionaries
+# Inspired from https://www.youtube.com/watch?v=hUyopAoOpG4
+def importDriversFile(path):
+    driversList = []
+    
+    with open(path, 'r') as file:
+        for line in file:
+            item = line.strip().split(', ')
+            
+            driver = {
+                'driverID': item[0],
+                'driverName': item[1],
+                'city': item[2]
+            }
+
+            driversList.append(driver)
+
+    return driversList
