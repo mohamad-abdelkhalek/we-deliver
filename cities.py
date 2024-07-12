6f609d263cf66d22e2a7c7065435d818d5d7324d
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-07-12 21:00:33 +0300

    <Add> function AddPath() to add paths between cities

--- cities.py
+++ cities.py
@@ -5,3 +5,10 @@ class Graph:
     def AddCity(self, city):
         if city not in self.graph:
             self.graph[city] = []
+            
+    def addPath(self, city1, city2):
+        if city1 in self.graph and city2 in self.graph:
+            self.graph[city1].append(city2)
+            self.graph[city2].append(city1)
+        else:
+            print("One or both cities", city1, city2, "are not in the graph.")
